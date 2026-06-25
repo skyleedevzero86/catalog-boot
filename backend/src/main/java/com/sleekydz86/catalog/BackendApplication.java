fa156ac9487a95da218865d6d20ae003d062ed18
이경용 <sleekydz86@naver.com>
2026-06-25 18:54:11 +0900

    feat: backend Project Init
    - backend 프로젝트 생성

---
+++ backend/src/main/java/com/sleekydz86/catalog/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/sleekydz86/catalog/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
